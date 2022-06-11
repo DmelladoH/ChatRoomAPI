0543bf422dc08269df6cbad76bbb9d236d69e5b6
DmelladoH <mellado.m2@gmail.com>
2022-06-11 16:38:40 +0200

    mongoDB implementation

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
+const { databaseConnection, databaseDisconnection } = require('./mongo')
+
 app.use(express.json())
 app.use(cors())
+
+databaseConnection(connectionURI, environment)
+
+process.on('uncaughtException', () => {
+  databaseDisconnection()
+})
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`)
